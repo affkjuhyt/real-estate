fb5b7367f56dfff22af0bd26f8e47104b23cf09c
affkjuhyt <nguyenthienlinhptit@gmail.com>
2025-04-18 12:15:15 +0700

    Translate for ground construction

--- src/app/[locate]/ground-up-construction/page.tsx
+++ src/app/[locate]/ground-up-construction/page.tsx
@@ -2,12 +2,20 @@ import Financing from "@/components/common/Financing";
 import GroupUpConstructionSection from "@/components/content/GroupUpConstructionSection";
 import FaqGroupUpConstructionSection from "@/components/faq/FaqGroupUpConstructionSection";
 
-export default function GroundUpConstruction() {
+async function fetchMessages(locale: string) {
+    const response = await import(`../../../../messages/${locale}.json`);
+    return response.default;
+}
+
+export default async function GroundUpConstruction({ params }) {
+    const resolvedParams = await params;
+    const messages = await fetchMessages(resolvedParams.locate);
+
     return (
         <div>
-            <GroupUpConstructionSection></GroupUpConstructionSection>
-            <FaqGroupUpConstructionSection></FaqGroupUpConstructionSection>
-            <Financing></Financing>
+            <GroupUpConstructionSection messages={messages}></GroupUpConstructionSection>
+            <FaqGroupUpConstructionSection messages={messages}></FaqGroupUpConstructionSection>
+            <Financing messages={messages}></Financing>
         </div>
     )
 }
--- src/components/content/GroupUpConstructionSection.tsx
+++ src/components/content/GroupUpConstructionSection.tsx
@@ -2,71 +2,11 @@
 
 import { useRouter } from "next/navigation";
 
-const loanTerms = [
-  {
-    title: "PROPERTY TYPE",
-    content: "Homes with 1-4 Units (5 units and above on a case by case basis)",
-  },
-  {
-    title: "LOAN AMOUNT",
-    content:
-      "UP TO US$ 5,000,000 (Loan amount above US $5M subject to longer approval time and additional requirements)",
-  },
-  {
-    title: "TYPE OF LOAN",
-    content:
-      "Interest only short term loan with a balloon payment at maturity.",
-  },
-  {
-    title: "MAXIMUM LOAN TO COST",
-    content: "Up to 85%",
-  },
-  {
-    title: "MAXIMUM LOAN TO ARV",
-    content: "70%",
-  },
-  {
-    title: "MAXIMUM LOAN TO VALUE",
-    content: "Up to 75%",
-  },
-  {
-    title: "BASE RATE",
-    content: "STARTING RATE 8.75% (Interest only payments on disbursed amount)",
-  },
-  {
-    title: "MINIMUM FICO SCORE",
-    content: "700",
-  },
-  {
-    title: "PREPAYMENT PENALTIES",
-    content: "NO PREPAYMENT PENALTIES 10-14 DAYS CLOSINGS",
-  },
-  {
-    title: "FOREIGN NATIONALS ACCEPTED",
-    content: "+ 0.25 to base rate",
-  },
-  {
-    title: "EASY DRAW PROCESS",
-    content: "",
-  },
-];
-
-const loanRequirements = [
-  "3 months of bank statements",
-  "ID, passport, (US visa for foreign nationals)",
-  "Entity Documents (LLC or Corp), EIN & OP Agreement",
-  "Rehab budget (if applicable)",
-  "6 months liquidity/reserves",
-  "Non-owner occupancy certification",
-  "SSN for credit report and background check",
-  "Evidence of insurance (full replacement + 500K liability) & builder's risk insurance (if applicable)",
-  "Minimum Fico score of 700",
-  "Signed loan application",
-  "Purchase Contract (if applicable.)",
-];
-
-export default function GroupUpConstructionSection() {
+export default function GroupUpConstructionSection({ messages}) {
   const router = useRouter();
+
+  const loanTerms = messages['ground_up_construction']['loan_terms'];
+  const loanRequirements = messages['ground_up_construction']['loan_requirements'];
 
   return (
     <div className="overflow-hidden bg-gray-100 py-24 sm:py-32">
@@ -75,38 +15,32 @@ export default function GroupUpConstructionSection() {
           <div className="lg:pr-8 lg:pt-4">
             <div className="lg:max-w-lg">
               <p className="text-xl mt-6 text-black font-bold uppercase">
-                WHAT IS A GROUND-UP CONSTRUCTION LOANS?
+                {messages['ground_up_construction']['title']}
               </p>
               <p className="mt-2 text-lg/8 text-gray-600">
                 <span className="font-semibold text-gray-700">
-                  Ground-Up Construction Loans
+                  {messages['ground_up_construction']['a_content']}
                 </span>{" "}
-                are short-term, specialized loans used to finance the building
-                of a new property from the ground up, rather than for purchasing
-                or renovating an existing structure. These loans are typically
-                used by developers or builders to cover the costs of land
-                acquisition, construction materials, labor, and other expenses
-                associated with building a new residential or commercial
-                property.
+                {messages['ground_up_construction']['b_content']}
               </p>
               <p className="mt-6 text-lg/8 text-gray-600">
-                At{" "}
+                {messages['ground_up_construction']['c_content']}{" "}
                 <span className="font-semibold text-gray-700">
                   Sapience Capital
                 </span>
-                , we tailor our programs to your experience and your project.
+                {messages['ground_up_construction']['d_content']}
               </p>
               <button
                 onClick={() => router.push("/apply-now")}
                 className="mt-8 bg-[#FA7F29] text-white px-6 py-2.5 rounded-md font-semibold hover:bg-[#e06a15] transition-colors hover:cursor-pointer"
               >
-                APPLY NOW
+                {messages['ground_up_construction']['apply_now']}
               </button>
             </div>
           </div>
           <div className="bg-gray-50 rounded-xl shadow-xl p-6 overflow-hidden">
             <h3 className="text-2xl font-semibold text-[#FA7F28] mb-6 uppercase">
-              GROUND UP CONSTRUCTION TERMS
+              {messages['ground_up_construction']['term']}
             </h3>
             <div className="overflow-y-auto max-h-[600px] pr-4 space-y-6">
               {loanTerms.map((term, index) => (
